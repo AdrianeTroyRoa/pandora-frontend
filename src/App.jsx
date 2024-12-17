4cf1dc91c850406076cc02ef38a7fb8a9d435222
Adriane <roatroy850@gmail.com>
2024-12-18 02:28:19 +0800

    feat: added login and register pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import Splash from './pages/Splash.jsx'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
 //import styles from './App.module.css';
 
 function App() {
   return (
-      <Splash />
+      <Register />
   );
 }
 
