a80b72a5f51712908afb97281565d99bdbad0d2e
Adriane <roatroy850@gmail.com>
2024-12-20 15:20:55 +0800

    feat: added 'axios' for backend communication

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import logo from './logo.svg';
-import Splash from './pages/Splash.jsx'
-import Login from './pages/Login.jsx'
-import Register from './pages/Register.jsx'
+import logo from "./logo.svg";
+import Splash from "./pages/Splash.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import apiClient from "./apiClient";
+import { createSignal } from "solid-js";
 //import styles from './App.module.css';
 
 function App() {
-  return (
-      <Register />
-  );
+  const [greet, setGreet] = createSignal(null);
+
+  apiClient.get("/greet").then((response) => {
+    setGreet(response.data);
+  });
+
+  return <h1>{greet()}</h1>;
 }
 
 export default App;
