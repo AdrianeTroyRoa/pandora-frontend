3c6a8818a4b881613b01b951f860504df82539d6
Adriane <roatroy850@gmail.com>
2024-12-26 16:01:50 +0800

    feat: started modify old AdminProducts component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import Splash from './pages/Splash.jsx'
+import AdminProducts from './pages_admin/AdminProducts';
 //import styles from './App.module.css';
 
 function App() {
   return (
-      <Splash />
+      <AdminProducts />
   );
 }
 
